6924da60c8251c922e14a8d22e7a3715a38a5b53
James Wettenhall <James.Wettenhall@monash.edu>
2018-10-13 06:07:37 +1100

    Renaming 010_MIDDLEWARE_CLASSES.py to 010_MIDDLEWARE.py, moving SessionMiddleware after CommonMiddleware and CsrfViewMiddleware and using list instead of tuple.

---
+++ settings.d/010_MIDDLEWARE.py
@@ -0,0 +1,13 @@
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'tardis.tardis_portal.logging_middleware.LoggingMiddleware',
+    'tardis.tardis_portal.auth.token_auth.TokenAuthMiddleware',
+    'tardis.tardis_portal.filters.FilterInitMiddleware',
+]
